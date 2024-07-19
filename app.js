6764deb6eb6e7190482f44b5c226d1d41030fa6e
waltt3x <wsims606@gmail.com>
2024-07-19 16:56:00 +0000

    js

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict"
+
+ const input = document.querySelector(".input")
+
+ const result = document.querySelector(".result")
+
+ const deleteBtn = document.querySelector(".delete")
+
+ const keys = document.querySelectorAll(".bottom span")
+
+
+ let operation = ""
+
+ let answer 
+
+ let decimalAddec = false
+
+
+ const operators = ["+", "-", "x", "/"]
+
+ function handleKeyPress(e){}
